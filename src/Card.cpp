4b1087dc38b786553dc2dda73ce7db2421e21323
nma <24021380@vnu.edu.vn>
2025-03-21 23:26:53 +0700

    add card types, merging, card deck rendering

--- src/Card.cpp
+++ src/Card.cpp
@@ -4,54 +4,141 @@
 #include "Game.h"
 #include "Defs.h"
 
-Card::Card(const char* file)
-{
-    cardPos.w = 64;
-    cardPos.h = 96;
+int selectedCard = 0;
+Uint32 lastKeyPress = 0;
 
-    cardSprite = IMG_LoadTexture(Game::renderer, file);
-    if (cardSprite == NULL)
+Card::Card(CardType t, int s) : type(t), star(s)
+{
+    position.w = 64;
+    position.h = 96;
+
+    const char* file;
+    if (type == ATTACK)
+    {
+        switch(star)
+        {
+            case 1: file = "assets/card_attack01.png"; break;
+            case 2: file = "assets/card_attack02.png"; break;
+            case 3: file = "assets/card_debuff.png"; break;
+        }
+
+    }
+    else if (type == HEAL)
+    {
+        switch(star)
+        {
+            case 1: file = "assets/card_heal01.png"; break;
+            case 2: file = "assets/card_heal02.png"; break;
+            case 3: file = "assets/card_shield.png"; break;
+        }
+    }
+    sprite = IMG_LoadTexture(Game::renderer, file);
+    if (sprite == NULL)
     {
         std::cout << "Failed to load card sprite!" << IMG_GetError() << std::endl;
     }
+
+    updateEffect();
 }
 
-void Card::render()
+void Card::updateEffect()
 {
-    for (int i = 0; i < numCards; i++)
+    if (type == ATTACK)
     {
-        SDL_Rect desRect = {64 + i * (cardPos.w + 32), SCREEN_HEIGHT - cardPos.h - 64, cardPos.w, cardPos.h};
-        if (i == selectedCard)
+        switch(star)
         {
-            SDL_SetRenderDrawColor(Game::renderer, 255, 255, 255, 255);
-            SDL_RenderFillRect(Game::renderer, &desRect);
-            SDL_SetRenderDrawColor(Game::renderer, 0, 0, 0, 0);
+            case 1: effect = "Basic Attack"; break;
+            case 2: effect = "Upgraded Attack"; break;
+            case 3: effect = "Stun Debuff"; break;
         }
-        else
+    }
+    else if (type == HEAL)
+    {
+        switch(star)
         {
-            SDL_RenderCopy(Game::renderer, cardSprite, NULL, &desRect);
+            case 1: effect = "Basic Heal"; break;
+            case 2: effect = "Upgraded Heal"; break;
+            case 3: effect = "Shield"; break;
         }
     }
 }
 
-void Card::cardSelection(SDL_Event &event, Enemy* enemy)
+SDL_Rect Card::getPos()
+{
+    return position;
+}
+
+Card* getRandomCard()
+{
+    CardType type = (rand() % 2) ? ATTACK : HEAL;
+    int star = 1;
+    return new Card(type, star);
+}
+
+void Card::render(SDL_Rect &destRect)
+{
+    SDL_RenderCopy(Game::renderer, sprite, NULL, &destRect);
+}
+
+void renderDeck(Card* deck[])
+{
+    for (int i = 0; i < TOTAL_CARDS; i++)
+    {
+        int w = deck[i]->getPos().w;
+        int h = deck[i]->getPos().h;
+        SDL_Rect destRect = {64 + i * (w + 16), SCREEN_HEIGHT - h - 64, w, h};
+        if (i == selectedCard)
+        {
+            SDL_SetRenderDrawColor(Game::renderer, 255, 255, 255, 255);
+            SDL_RenderFillRect(Game::renderer, &destRect);
+            SDL_SetRenderDrawColor(Game::renderer, 0, 0, 0, 0);
+            // de tam hinh trang
+        }
+        else
+        {
+            deck[i]->render(destRect);
+        }
+    }
+}
+
+void handleCardEvents(SDL_Event &event, Enemy* enemy, Card* deck[])
 {
     Uint32 currentTime = SDL_GetTicks();
     if ((event.type == SDL_KEYDOWN) && (currentTime - lastKeyPress > 150))
     {
         switch(event.key.keysym.sym)
         {
-            case SDLK_LEFT: selectedCard = (selectedCard - 1 + numCards) % numCards; break;
-            case SDLK_RIGHT: selectedCard = (selectedCard + 1) % numCards; break;
-            case SDLK_RETURN: useCard(selectedCard, enemy); break;
+            case SDLK_LEFT: selectedCard = (selectedCard - 1 + TOTAL_CARDS) % TOTAL_CARDS; break;
+            case SDLK_RIGHT: selectedCard = (selectedCard + 1) % TOTAL_CARDS; break;
+            case SDLK_RETURN: deck[selectedCard]->useCard(selectedCard, enemy); break;
+            case SDLK_SPACE:
+                if (selectedCard < TOTAL_CARDS - 1 && deck[selectedCard]->canMerge(deck[selectedCard + 1]))
+                {
+                    deck[selectedCard]->mergeWith(deck[selectedCard + 1]);
+                }
         }
         lastKeyPress = currentTime;
     }
 }
 
-void Card::useCard(int cardNum, Enemy* enemy)
+void Card::useCard(int cardIndex, Enemy* enemy)
 {
     enemy->takeDamage(5);
-    std::cout << "Use card number " << cardNum << std::endl;
+    std::cout << "Use card number " << cardIndex << std::endl;
     std::cout << "Enemy HP: " << enemy->health << std::endl;
+}
+
+bool Card::canMerge(Card* other)
+{
+    return (other != NULL && type == other->type && star == other->star && star < 3);
+}
+
+void Card::mergeWith(Card* other)
+{
+    if (canMerge(other))
+    {
+        star++;
+        updateEffect();
+        other->star = 0;
+    }
 }
