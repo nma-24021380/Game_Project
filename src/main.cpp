925b186d3ae447fa95f49eabb0aa4f5b17949684
nma <24021380@vnu.edu.vn>
2025-03-21 23:14:13 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include "Game.h"
 #include "Defs.h"
+#include <ctime>
+#include <cstdlib>
 
 Game* game = NULL;
 
 int main(int argc, char* argv[])
 {
+    srand(time(0));
+
     game = new Game();
     game->init("Game Project", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, false);
     while (game->running())
